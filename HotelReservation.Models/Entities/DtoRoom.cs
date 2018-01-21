78522310d513d978b728c1a1c78b68a45ee9e47d
Abdelrhman Shawky <abdelrhmanshawky4@gmail.com>
2018-01-21 18:48:22 +0100

    removed column floor

--- HotelReservation.Models/Entities/DtoRoom.cs
+++ HotelReservation.Models/Entities/DtoRoom.cs
@@ -3,7 +3,6 @@
     public class DtoRoom : BaseEntity
     {
         public string Number { get; set; }
-        public double Floor { get; set; }
         public DtoRoomType RoomType { get; set; }
     }
 }
