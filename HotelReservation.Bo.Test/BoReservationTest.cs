647bf30e89da15d72a9c5b3c719305bbdd306dfd
Abdelrhman Shawky <abdelrhmanshawky4@gmail.com>
2018-01-21 16:18:28 +0100

    Revert "Implement test case for BoReservationTest"
    This reverts commit 5b88c91332893acdac1a33410232ee7e874001f2.

--- HotelReservation.Bo.Test/BoReservationTest.cs
+++ HotelReservation.Bo.Test/BoReservationTest.cs
@@ -1,39 +1,16 @@
 ﻿using System;
-using System.Linq;
-using HotelReservation.Data.Common;
-using HotelReservation.Data.Repositories.Interfaces;
-using HotelReservation.Models.Entities;
-using Moq;
 using NUnit.Framework;
-using Unity.Lifetime;
-using Unity.Registration;
 
 namespace HotelReservation.Bo.Test
 {
     [TestFixture]
     public class BoReservationTest
     {
-        [SetUp]
-        public void Setup()
+        [TestCase(1)]
+        [TestCase(2)]
+        public void BookReservation_Test(int num)
         {
-            //UnityConfiguration.RegisterTypes();
-            var reservationRepositoryMocked = new Mock<IReservationRepository>();
-            UnityConfiguration.Container.RegisterInstance(typeof(IRepository<DtoReservation>), "", reservationRepositoryMocked.Object, new PerThreadLifetimeManager());
-            reservationRepositoryMocked.Setup(x => x.Insert(It.IsAny<DtoReservation>())).Returns<DtoReservation>(x => x);
-        }
-
-        [Test]
-        public void BookReservation_Test()
-        {
-            //Arrange
-            var boReservation = new BoReservation();
-            
-            //Act
-            var dtoReservation = boReservation.Book(new DtoGuest(), new DtoRoom {RoomType = new DtoRoomType()}, DateTime.Now,
-                DateTime.Now.AddDays(1));
-            
-            //Assert
-            Assert.IsTrue(dtoReservation.ReservationStatusList.Select(x=>x.ReservationStatus).Contains(ReservationStatus.Booked));
+            Assert.AreEqual(1,num);
         }
     }
 }
